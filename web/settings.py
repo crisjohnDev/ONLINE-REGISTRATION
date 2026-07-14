ee74614bfcf5b36247ff4e5c5a9e4f66bbb44f70
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-07-15 01:00:06 +0800

    files for requirements

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wo@c-nzhc5$%d*bwa=n#47dugzx)%6de69o)hss^c#m%wucir6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
